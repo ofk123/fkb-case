93ac97f295d86c966073c92d75d8e149de3200d9
Ola Kihle <ola.formo.kihle@outlook.com>
2024-11-01 02:44:09 -0700

    Noen endringer
    - return geopandas-df from error_geometries()
    - Visualize

--- ola/case.py
+++ ola/case.py
@@ -21,4 +21,6 @@ def error_geometries(geodf, outfile, max_distance=1.0):
             if geom_points[i].distance(geom_points[j]) <= max_distance:
                 wrong += [i_points[i], i_points[j]]
 
-    return geodf.loc[np.unique(np.array(wrong)).tolist()].to_file(outfile, driver='GeoJSON')
+    geodf_wrong = geodf.loc[np.unique(np.array(wrong)).tolist()]
+    geodf_wrong.to_file(outfile, driver='GeoJSON')
+    return geodf_wrong
